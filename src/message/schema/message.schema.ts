0d9360cb0cb69409683515d741b70d58ec64cb01
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-09 18:50:53 +0100

    Fix : refactor schema of message

--- src/message/schema/message.schema.ts
+++ src/message/schema/message.schema.ts
@@ -1,20 +1,23 @@
-import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
 import { User } from 'src/user/schema/user.schema';
+import { Channel } from '../../channel/schemas/channel.schema';
+
+export type MessageDocument = Message & Document;
 
 @Schema({ timestamps: true })
 export class Message {
   @Prop({ required: true, type: Types.ObjectId, ref: User.name })
-  sender: Types.ObjectId;  
+  sender: Types.ObjectId;
 
   @Prop({ required: true })
-  text: string;  
+  text: string;
 
-  @Prop({ required: true, type: Types.ObjectId, ref: 'Conversation' })
-  conversation: Types.ObjectId;  
+  @Prop({ type: Types.ObjectId, ref: Channel.name, required: true })
+  channel: Types.ObjectId;
 
-//   @Prop({ default: false })
-//   isEdited: boolean;  
+  @Prop({ required: true, type: Date, default: Date.now })
+  createdAt: Date;
 }
 
-export const MessageSchema = SchemaFactory.createForClass(Message)
+export const MessageSchema = SchemaFactory.createForClass(Message);
