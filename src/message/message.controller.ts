90845130cb5f9a74593baa43a46cefc0e9641626
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-09 18:58:52 +0100

    Add : add new endpoint in controller message

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Param } from '@nestjs/common';
+import { MessageService } from './message.service';
+import { Message } from './schema/message.schema';
 
-@Controller('message')
-export class MessageController {}
+@Controller('messages')
+export class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
+  @Post('channels/:channelId')
+  async sendMessage(
+    @Param('channelId') channelId: string,
+    @Body('userId') userId: string,
+    @Body('text') text: string,
+  ): Promise<Message> {
+    return this.messageService.sendMessage(channelId, userId, text);
+  }
+}
