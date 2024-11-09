2e71e53890c1c7f8bd06674029e82b8f08f31bb0
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-09 11:13:14 +0100

    update : update channel modules

--- src/channel/channel.module.ts
+++ src/channel/channel.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ChannelController } from './channel.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChannelService } from './channel.service';
+import { ChannelController } from './channel.controller';
+import { Channel, ChannelSchema } from './schemas/channel.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Channel.name, schema: ChannelSchema }])
+  ],
   controllers: [ChannelController],
-  providers: [ChannelService]
+  providers: [ChannelService],
+  exports: [ChannelService],
 })
 export class ChannelModule {}
+
