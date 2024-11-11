a41ac6dddc9d9da4a47eb71c81e0fc97034e1f96
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-11 10:51:43 +0100

    Fix : fixing features WEBRTC

--- src/RTC/webrtc.gateway.ts
+++ src/RTC/webrtc.gateway.ts
@@ -28,24 +28,27 @@ export class WebRtcGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   @SubscribeMessage('offer')
   handleOffer(
     client: Socket,
-    payload: { offer: RTCSessionDescriptionInit; to: string },
+    payload: { offer: string; to: string },
   ) {
+    console.log(`Offer received from client ${client.id}: ${payload.offer}`);
     this.server.to(payload.to).emit('offer', { offer: payload.offer, from: client.id });
   }
 
   @SubscribeMessage('answer')
   handleAnswer(
     client: Socket,
-    payload: { answer: RTCSessionDescriptionInit; to: string },
+    payload: { answer: string; to: string },
   ) {
+    console.log(`Answer received from client ${client.id}: ${payload.answer}`);
     this.server.to(payload.to).emit('answer', { answer: payload.answer, from: client.id });
   }
 
   @SubscribeMessage('ice-candidate')
   handleIceCandidate(
     client: Socket,
-    payload: { candidate: RTCIceCandidateInit; to: string },
+    payload: { candidate: string; to: string },
   ) {
+    console.log(`ICE Candidate received from client ${client.id}: ${payload.candidate}`);
     this.server.to(payload.to).emit('ice-candidate', { candidate: payload.candidate, from: client.id });
   }
 }
