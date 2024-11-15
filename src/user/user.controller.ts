ef70e619f7fde9fe797790e6664ed53031b5236b
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-15 17:07:55 +0100

    Fix : endpoints Getting all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './schema/user.schema';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/users')
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.GetAllUsers();
+  }
+
+
+}
