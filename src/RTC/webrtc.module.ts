3d46442c925477122fbd8cd5fcce7112da61947d
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-16 23:40:27 +0100

    Fix : Import message module in rtc Module

--- src/RTC/webrtc.module.ts
+++ src/RTC/webrtc.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { WebRtcGateway } from './webrtc.gateway';
 import { WebRtcService } from './webrtc.service';
 import { WebRtcController } from './webrtc.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from '../message/schema/message.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+  ],
   providers: [WebRtcGateway, WebRtcService],
-  controllers: [WebRtcController]
+  controllers: [WebRtcController],
 })
 export class WebRtcModule {}
